f18a583677f4448edd69797eccfd29d4faa243a4
Askhad Arzybaev <aarzybaev@gmail.com>
2024-01-18 23:48:52 +0600

    components created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import Appbar from './components/Appbar/Appbar';
+import UserForm from './components/UserForm/UserForm';
+import Users from './components/Users/Users';
+
 function App() {
 
   return (
     <>
-      Hello
+      <header>
+        <Appbar/>
+      </header>
+      <main className="container-fluid">
+        <div className="row mt-2">
+          <div className="col-6">
+            <UserForm/>
+          </div>
+          <div className="col-6">
+            <Users/>
+          </div>
+          </div>
+      </main>
     </>
   );
 }
